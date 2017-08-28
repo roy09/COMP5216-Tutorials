9e69b63d9b5cfb66c21b3efeba89bedd8e33b828
unknown <jroy2516@WGCJ4CD2.internal.usyd.edu.au>
2017-08-28 19:31:22 +1000

    first commit

---
+++ COMP5216W05/app/src/test/java/au/edu/uni/sydney/comp5216/comp5216w05/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package au.edu.uni.sydney.comp5216.comp5216w05;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
